33cb34798f4c5e1dadb9a6778925b995d8097348
tcong <tcong@appannie.com>
2017-09-12 11:42:08 +0800

    update host

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 db_user = 'postgres'
 db_pass = 'postgres'
-db_host = 'localhost'
+db_host = 'postgres'
 db_port = 5432
 db_name = 'postgres'
 
