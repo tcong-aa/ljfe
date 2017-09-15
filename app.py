f4d34ffaa38f80a565c6095963a120799f3975ff
tcong <tcong@appannie.com>
2017-09-15 14:41:38 +0800

    add apis

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, make_response
 
-from flask import render_template
+from flask import render_template, request
 
 from extensions import db
+
+from week_data_converter.converter import WeekPrice, MonthPrice
 
 import logging
 logger = logging.getLogger(__name__)
@@ -27,6 +29,52 @@ def register_blueprints(app):
     @app.route('/')
     def index():
         return render_template('index.html', title='Hommy Candle Stick')
+
+    @app.route('/month_prices')
+    def week_by_code():
+
+        code = request.args.get('code')
+
+        month_prices = db.session.query(MonthPrice).filter(MonthPrice.community_code==code).all()
+
+        prices_rows = []
+
+        for month_price in month_prices:
+            prices_rows.append({
+                "open": month_price.open,
+                "close": month_price.close,
+                "high": month_price.high,
+                "low": month_price.low,
+                "volumn": month_price.volumn
+            })
+
+        return jsonify({
+            "code": code,
+            "prices": prices_rows
+        })
+
+    @app.route('/week_prices')
+    def week_by_code():
+
+        code = request.args.get('code')
+
+        week_prices = db.session.query(WeekPrice).filter(WeekPrice.community_code==code).all()
+
+        prices_rows = []
+
+        for week_price in week_prices:
+            prices_rows.append({
+                "open": week_price.open,
+                "close": week_price.close,
+                "high": week_price.high,
+                "low": week_price.low,
+                "volumn": week_price.volumn
+            })
+
+        return jsonify({
+            "code": code,
+            "prices": prices_rows
+        })
 
     @app.errorhandler(404)
     def not_found(error):
